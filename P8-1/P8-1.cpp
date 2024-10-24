8f36aca391e1fdbc1d99e256fc76420e1d3c0b1f
Adam Miller <a-miller.33@onu.edu>
2024-10-24 13:21:05 -0400

    Completed countVowel function

--- P8-1/P8-1.cpp
+++ P8-1/P8-1.cpp
@@ -2,18 +2,23 @@
 #include <string>
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 
 int main() {
 	string str;
 	cout << "Enter a string: ";
 	cin >> str;
-	cout << "Count: " << countCharacter(str);
+	cout << "Count: " << countVowel(str);
 
 	return 0;
 }
 
-int countCharacter(string str) {
-	int count = str.length();
+int countVowel(string str) {
+	int count = 0;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+			count++;
+		}
+	}
 	return count;
 }
